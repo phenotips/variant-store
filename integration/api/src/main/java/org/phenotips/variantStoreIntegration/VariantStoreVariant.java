cd8e2039bd6e4e586eb7db4f8f6ff96f6aff1c90
Orion Buske <orion@phenotips.org>
2017-03-06 19:05:19 -0500

    [misc] Fixed wrong StringUtils import

--- integration/api/src/main/java/org/phenotips/variantStoreIntegration/VariantStoreVariant.java
+++ integration/api/src/main/java/org/phenotips/variantStoreIntegration/VariantStoreVariant.java
@@ -24,7 +24,7 @@ import org.phenotips.variantstore.shared.VariantUtils;
 
 import java.util.List;
 
-import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.ga4gh.GACall;
 import org.ga4gh.GAVariant;
 
